4d0fe5fc8c9d993812822b012ec762a7e8fc28f1
Yunshasha <syun@red.software.systems>
2024-03-14 14:08:56 +0100

    update Pressable.js

--- components/Pressable.js
+++ components/Pressable.js
@@ -6,12 +6,20 @@ function PressableTest() {
   let textLog = "";
   if (timesPressed > 1) {
     textLog = timesPressed + "x onPress";
-  } else {
+  } else if (timesPressed > 0) {
     textLog = "onPress";
   }
   return (
-    <View>
-      <Pressable></Pressable>
+    <View style={styles.container}>
+      <Pressable
+        onPress={() => SetTimesPressed((cur) => cur + 1)}
+        style={({ pressed }) => [
+          { backgroundColor: pressed ? "rgb(210,230,255)" : "white" },
+          styles.wrapperCustom,
+        ]}
+      >
+        {({ pressed }) => <Text>{pressed ? "Pressed!" : "Press Me"}</Text>}
+      </Pressable>
 
       <View style={styles.logBox}>
         <Text>{textLog}</Text>
@@ -28,6 +36,14 @@ const styles = StyleSheet.create({
     borderColor: "#f0f0f0",
     backgroundColor: "#f9f9f9",
     borderCurve: 3,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    margin: 20,
+  },
+  wrapperCustom: {
+    borderRadius: 8,
   },
 });
 
